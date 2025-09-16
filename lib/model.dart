0456b60edd4c9c4c0783ea9c3556386c4a577a04
KristofferGus <109553487+mrSWE3@users.noreply.github.com>
2025-09-16 17:13:23 +0200

    Made TodoItem into ChangeNotifier

--- lib/model.dart
+++ lib/model.dart
@@ -1,38 +1,48 @@
 import 'package:flutter/material.dart';
+
 //Imutable with safe copy todo item
-class TodoItem{
-  final bool _done;
-  final String _text;
-  // ignore: unnecessary_string_interpolations
-  TodoItem(String text, {bool done = false}): _text = "$text", _done = done;
+class TodoItem extends ChangeNotifier{
+  bool _done;
+  String _text;
+  TodoItem(String text, {bool done = false}) : _text = "$text", _done = done;
   bool get done => _done;
-  // ignore: unnecessary_string_interpolations
+  set done(bool value){
+    _done = value;
+    notifyListeners();
+  }
   String get text => "$_text";
+  set text(String value){
+    _text = "$value";
+    notifyListeners();
+  }
 }
 
-class TodoItemsState extends ChangeNotifier{
+class TodoItemsState extends ChangeNotifier {
   final List<TodoItem> _items = [];
   int get length => _items.length;
 
-  void add(TodoItem item){
+  void add(TodoItem item) {
     _items.add(item);
     notifyListeners();
-  } 
+  }
 
-  void remove(TodoItem item){
+  void remove(TodoItem item) {
     _items.remove(item);
     notifyListeners();
   }
 
-  void removeAt(int index){
+  void removeAt(int index) {
     _items.removeAt(index);
     notifyListeners();
   }
 
-  TodoItem getItem(int index){
+  TodoItem getItem(int index) {
     return _items[index];
   }
 
   TodoItem operator [](int index) => _items[index];
-}
 
+  void operator []=(int index, TodoItem item) {
+    _items[index] = item;
+  }
+}
